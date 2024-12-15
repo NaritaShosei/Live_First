5520840eb74958142e0ac082098d332483257c96
NaritaShosei <onigirikun7171@gmail.com>
2024-12-16 01:33:42 +0900

    オクルージョンカリングとペンライトをふるアニメーション

--- Assets/Scripts/PenLights.cs
+++ Assets/Scripts/PenLights.cs
@@ -6,19 +6,20 @@ public class PenLights : MonoBehaviour
 {
     [SerializeField] List<Material> _materialList = new();
     [SerializeField] GameObject _stick;
-    [SerializeField] Vector3 _stickPos = new Vector3(15, 0, 30);
+    [SerializeField] Vector3 _stickPos = new(15, 0, 30);
+    List<GameObject> _stickList = new();
     void Start()
     {
         StartStick();
     }
 
-    async void StartStick()
+    void StartStick()
     {
         for (float i = 0; i < _stickPos.x; i += 0.5f)
         {
             for (float j = 0; j < _stickPos.z; j += 0.5f)
             {
-                await InstantiateAsync(_stick, transform.position + new Vector3(i, 0, j), Quaternion.identity);
+                _stickList.Add(Instantiate(_stick, transform.position + new Vector3(i, 0, j), Quaternion.identity, transform));
             }
         }
     }
