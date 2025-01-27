3193bdb482d200bf7684903a2f5e457d52be3424
NaritaShosei <onigirikun7171@gmail.com>
2025-01-27 18:25:50 +0900

    [update]QTE(仮称)のタイミングをTimeLineから取得

---
+++ Assets/Scripts/QuickTimeEvent.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class QuickTimeEvent : MonoBehaviour
+{
+    Queue<float> _eventQueue = new();
+    [SerializeField] TimelineAsset _timelineAsset;
+    [SerializeField] PlayableDirector _playableDirector;
+    [SerializeField, Header("ƒm[ƒc‚Ìƒgƒ‰ƒbƒN‚Ì–¼‘O")] string _trackName;
+    [SerializeField, Header("”»’è‚Ì—P—\ŽžŠÔ")] float _graceTime = 0.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (var track in _timelineAsset.GetOutputTracks())
+        {
+            foreach (var clip in track.GetClips())
+            {
+                Debug.Log(clip.displayName);
+                if (_trackName == clip.displayName)
+                {
+                    _eventQueue.Enqueue((float)clip.start);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_eventQueue.Count > 0)
+        {
+            if (_playableDirector.time <= _eventQueue.Peek() + _graceTime)
+            {
+
+            }
+        }
+    }
+}
